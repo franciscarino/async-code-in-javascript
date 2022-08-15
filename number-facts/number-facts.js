be0b4460b698a15fa4e254c88967b83a604b7888
Francis Carino <fgcarino@gmail.com>
2022-08-15 16:33:00 -0700

    complete part 1

---
+++ number-facts/number-facts.js
@@ -0,0 +1,88 @@
+"use strict";
+
+//PART 1
+
+const BASE_URL = "http://numbersapi.com/";
+
+async function getFavoriteNumber() {
+  const response = await axios({
+    url: `${BASE_URL}100?json`,
+    method: "GET",
+  });
+
+  return response.data;
+}
+
+async function addFavNumToPage() {
+  const favNum = await getFavoriteNumber();
+
+  console.log(favNum);
+
+  $("body").text(favNum.text);
+}
+
+addFavNumToPage();
+
+//PART 2
+
+async function getAllFavoriteNumbers() {
+  const response = await axios({
+    url: `${BASE_URL}11,12,13,14?json`,
+    method: "GET",
+  });
+
+  return response.data;
+}
+
+async function addAllFavNumToPage() {
+  const favNum = await getAllFavoriteNumbers();
+
+  console.log(favNum);
+
+  for (let num in favNum) {
+    $("body").append(`<p>${favNum[num]}</p>`);
+  }
+}
+
+addAllFavNumToPage();
+
+//PART 3
+
+async function get4Facts() {
+  const response1 = axios({
+    url: `${BASE_URL}10?json`,
+    method: "GET",
+  });
+  const response2 = axios({
+    url: `${BASE_URL}10?json`,
+    method: "GET",
+  });
+  const response3 = axios({
+    url: `${BASE_URL}10?json`,
+    method: "GET",
+  });
+  const response4 = axios({
+    url: `${BASE_URL}10?json`,
+    method: "GET",
+  });
+
+  let results = await Promise.allSettled([
+    response1,
+    response2,
+    response3,
+    response4,
+  ]);
+  return results;
+}
+
+async function dispayGet4Facts() {
+  const results = await get4Facts();
+
+  for (let item of results) {
+    if (item.status === "fulfilled") {
+      $("body").append(`<p>${item.value.data.text}</p>`);
+    }
+  }
+}
+
+dispayGet4Facts();
